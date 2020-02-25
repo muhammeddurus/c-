277430cbfe5bde3d037c91758e820ce324eef944
Muhammed Duruş <muhammeduruss@gmail.com>
2020-02-25 10:56:37 +0300

    c# Radyo

---
+++ WindowsFormsApplication13/WindowsFormsApplication13/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = @" http://yayindaslowradyo.net:5050/;";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://37.59.205.232:6688/;stream.mp3 ";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = " http://server22692.streamplus.de/;stream.mp3 ";
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "https://17733.live.streamtheworld.com/EFKAR.mp3";
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = " https://17753.live.streamtheworld.com/JOYTURK_AKUSTIK.mp3";
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = " https://playerservices.streamtheworld.com/api/livestream-redirect/METRO_90LAR.mp3 ";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = " http://AskFm.CO:9300";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://dinle.slowkaradenizfm.com:9578/;";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://yayin.damarfm.com:8080/;";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://radyo.dogannet.tv/dreamturk";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = " http://fenomen.listenfenomen.com/fenomen/128/icecast.audio";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://turkiyemfm.canliyayinda.com:8032/;";
+        }
+    }
+}
